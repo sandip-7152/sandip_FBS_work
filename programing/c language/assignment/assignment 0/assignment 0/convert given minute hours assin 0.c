ca0a7b7191ca3454683ebfb84e7c7845010dfb3b
sandip rathod <sandiprathod41714@gmail.com>
2025-07-12 14:01:06 +0530

    Add files via upload

---
+++ programing/c language/assignment/assignment 0/assignment 0/convert given minute hours assin 0.c
@@ -0,0 +1,9 @@
+//write a c program convert given minute into hours and remaining minutes
+#include<stdio.h>
+void main()
+{
+	int minut,houre,remaining,minutes;
+	printf("enter total minutes:");
+	scanf("%d",&minutes);
+	printf("%d minutes is equal to %d hour(s)   and %d minute(s)\n",minutes,houre,remaining,minutes);
+}
---
+++ programing/c language/assignment/assignment 0/assignment 0/find squqre and cuble number asi 0.c
@@ -0,0 +1,10 @@
+//write a c program to find the square and cuble of a given number
+#include<stdio.h>
+void main()
+{
+	int number,square,cuble;
+	printf("enter a number:");
+	scanf("%d",&number);
+	printf("square of %d is:%d\n",number,square);
+	printf("cuble of %d is:%d\n",number,cuble);
+}
