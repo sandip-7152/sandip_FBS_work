fac0a602d8c162ec4483d2bf431958f3c18c7546
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:16:03 +0530

    Add files via upload

---
+++ programing/c language/assignment/swap two number assi 0.c
@@ -0,0 +1,11 @@
+//writera c program to swap two number using a termporary third varible 
+#include<stdio.h>
+void main()
+{
+	int a,b,temp;
+	printf("enter first number (a):");
+	scanf("%d",&a);
+	printf("enter second number (b):");
+	scanf("%d",&b);
+	printf("after swapping:a=%d,b= %d\n",a,b);
+}
