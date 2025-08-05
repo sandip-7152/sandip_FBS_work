9aee862f5978f1ed587670cc3819fa0286a541a1
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:13:12 +0530

    Add files via upload

---
+++ c programming/assingment 4/given range num assi 4_1.c
@@ -0,0 +1,15 @@
+//print prime numbers in the given range 1 to n
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the value of n :");
+	scanf("%d",&n);
+	printf("perfect numbers form 1 to % are:\n",n);
+for(int i=1; i<=n; i++)
+	{
+	printf("perfect numbers form 1 to % are:\n",n);
+	for(int j=1;j<=n;j++)
+	printf("%d",&n);
+}
+}
