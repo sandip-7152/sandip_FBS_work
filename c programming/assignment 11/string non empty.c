f21f474b0bb2f34bd8766ba688c07885449176ce
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:42:00 +0530

    Add files via upload

---
+++ c programming/assignment 11/string non empty.c
@@ -0,0 +1,16 @@
+//WAP to Remove the nth Index Character from a Non-Empty String
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[200];
+	int index;
+	printf("Enter a string");
+	scanf("%s",str);
+	printf("Enter index of character ");
+	scanf("%d",&index);
+	{
+	printf("Enter % index nth character froma non empty string");
+	printf("%d",str);
+}
+}
