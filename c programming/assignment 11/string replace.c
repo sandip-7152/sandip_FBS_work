65f78985802cccec829a18212f63290eb3480b86
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:42:57 +0530

    Add files via upload

---
+++ c programming/assignment 11/string replace.c
@@ -0,0 +1,18 @@
+//2. WAP Replace all Occurrences of ‘a’ with $ in a String
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+	char str[20];
+	int i,found=0;
+	printf("Enter a replace");
+	printf("Enter all Occurrences of ‘a’ with ");
+	scanf("%d",str);
+	for(i=0;i<strlen(str);i++)
+	{
+	
+		found=0;
+		break;
+	}
+printf("Replace all Occurrences of ‘a’ with $");
+}
