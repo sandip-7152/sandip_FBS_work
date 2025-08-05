ed74ba35c1b482ec04e1d615148e1154e4c36ce6
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:32:45 +0530

    Add files via upload

---
+++ c programming/assignment 7/take two add third .c
@@ -0,0 +1,21 @@
+//taketwo array and add sum in third array
+#include<stdio.h>
+void main()
+{
+	int arr[5],brr[5],crr[5];
+	printf("enter 5 number for the first array:\n");
+	for(int i=0;i<5;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	printf("enter 5 number for the second array:\n");
+	for(int i=1;i<5;i++)
+	{
+		scanf("%d",brr[i]);
+	}
+	printf("enter 5 number for the third array:\n");
+	for(int i=0;i<5;i++)
+	{
+		scanf("%d",crr[i]);
+	}
+}
