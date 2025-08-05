cbd8c8eca3cb075493518548650549640120d067
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:40:32 +0530

    Add files via upload

---
+++ c programming/assignment 11/string count .c
@@ -0,0 +1,23 @@
+//WAP to Count the Number of Vowels in a String
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[30];
+	int count;
+	printf("Enter a string");
+	scanf("%s",str);
+	printf("Enter count number of vowels");
+	scanf("%d",&count);
+	{
+		for(int i=0;i<count;i++)
+		{
+			if(str[0]==count)
+		{
+		break;
+		}
+		printf("Count the Number of Vowels in a String");
+		printf("%d",count);
+	}
+}
+}
