7af5f2ba9737ce9bc8a367427028b29e30f432fa
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:36:32 +0530

    Add files via upload

---
+++ c programming/assignment 9/in the prime number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	//prime numbrer is not prime number
+	int no=7;
+	for(int i=1;i<=1;i++)
+{
+printf("enter prime number");
+
+}
+	printf("prime number");
+	scanf("%d\n");
+{
+return;
+{
+for(int i=7;i<=1;i++)
+printf("enter the prime number");
+scanf("%d isprime num");
+}
+for(int i=7;i<=1;i++)
+i++;
+for(int j=7;j<=7;j--)
+{
+	printf("enter the prime number");
+}
+}
+}
