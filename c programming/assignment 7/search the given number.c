8436e903e354b93d3ce7ce4e572ac50930227f28
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:31:34 +0530

    Add files via upload

---
+++ c programming/assignment 7/search the given number.c
@@ -0,0 +1,17 @@
+//search the given number in array
+#include<stdio.h>
+void main()
+{
+	int num[23];
+	printf("enter search numbers \n");
+{
+	for(int i=0;i<23;i++)
+
+scanf("%d",&num[i]);
+}
+	printf("enter search numbers \n");
+	for(int i=0;i<23;i++)
+	scanf("%d",&num[i]);
+	printf("enter search numbers\n");
+}
+
