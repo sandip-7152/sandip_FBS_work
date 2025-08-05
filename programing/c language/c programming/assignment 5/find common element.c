1e0f41c4efd7847ca0fcfedb6806b6d74b9f8c93
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:20:02 +0530

    Add files via upload

---
+++ programing/c language/c programming/assignment 5/find common element.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n1,n2;
+	printf("enter the of first array");
+	scanf("%d",n1);
+for(int i=1;i<=n1;i++)
+{
+for(int j=1;j<=n2;j++)
+	printf("enter the of second array");
+	scanf("%d",n2);
+printf("\n");
+}
+}
+//write aprogram in c find two maximum number in array
+//write a program in c for to find common elements in two arrays
