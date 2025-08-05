c6dcc2c6aa33f674dbfa1f20c7b4890e1deefa0a
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:32:06 +0530

    Add files via upload

---
+++ c programming/assignment 7/sumnumber of all number.c
@@ -0,0 +1,19 @@
+//find the sum of all numbers
+#include<stdio.h>
+void main()
+{
+	int sum[8];
+	printf("enter sum of numbers /n");
+	{
+		for(int i=1;i<8;i++)
+		scanf("%d",&sum[i]);
+	}
+	printf("enter sum of numbers/n");
+	{
+		for(int i=1;i<8;i++)
+		scanf("%d",&sum[i]);
+	}
+	printf("enter sum of numbers /n");
+}
+	
+		
