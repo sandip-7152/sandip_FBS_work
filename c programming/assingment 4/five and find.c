9aadfdaa703e747319f5f98b46495ae645a1c740
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:11:35 +0530

    Add files via upload

---
+++ c programming/assingment 4/five and find.c
@@ -0,0 +1,13 @@
+
+//write a c program to input five number and find their average
+#include<stdio.h>
+void main()
+{
+	int num1,num2,nm3,num4,num5;
+	printf("enter the five number:\n");
+	scanf("%d",& num5);
+{
+printf("the ave is: %.5\n num");
+return;
+}
+}
