c3074e58f7edbc7f6a07523d79328f41ef1d7fac
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:40:03 +0530

    Add files via upload

---
+++ c programming/assignment 11/calculate.c
@@ -0,0 +1,16 @@
+//WAP to Calculate the Number of Words Present in a String
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str [200];
+	int calculate=0;
+	printf("Enter a string");
+	scanf("%s",str);
+
+{
+	printf("number of words in the string");
+	scanf("%d",calculate);
+	return 0;
+}
+}
