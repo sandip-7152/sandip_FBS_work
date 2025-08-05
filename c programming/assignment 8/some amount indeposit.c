1bac8094e8b877f640c8fd9193894b10edc85b30
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:34:53 +0530

    Add files via upload

---
+++ c programming/assignment 8/some amount indeposit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	float balance=5000;
+	float deposit,widthrol, amount;
+	printf("enter the  deposit ");
+	scanf("%d",deposit);
+	printf("enter the widthrol");
+	scanf("%d",widthrol);
+	{
+		printf("enter amount to withdraw");
+		scanf("%f",&amount);
+		printf("updated balance");
+	}
+	printf("\n");
+}
