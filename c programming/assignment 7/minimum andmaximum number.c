7eb09fe5d4ea4deb0c417d1bf0da8f5690f13899
sandip rathod <sandiprathod41714@gmail.com>
2025-08-05 21:31:00 +0530

    Add files via upload

---
+++ c programming/assignment 7/minimum andmaximum number.c
@@ -0,0 +1,21 @@
+//writer findthe minimum and maximum number in array
+#include<stdio.h>
+void main()
+{
+	int min[10];
+	printf("enter 10 minimum numbers \n");
+	for(int i=0;i<10;i++)
+	{
+		scanf("%d",&min[i]);
+	}
+	printf("you entered (min[10]):%d\n,min[9]");
+
+int max[5];
+printf("enter 5 maximum numbers\n");
+for(int i=0;i<1;i++)
+{
+	scanf("%d",&max[i]);
+
+printf("you entered (max[5]):%d\n,max[4]");
+}
+}
