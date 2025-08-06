7e5be2a26d380ecb95de79067ead33211a11dfa0
sandip rathod <sandiprathod41714@gmail.com>
2025-08-06 15:24:29 +0530

    Add files via upload

---
+++ c programming/test5/selling number test 5.c
@@ -0,0 +1,11 @@
+//1wap to calculate sellng price of book based on cost price and discount (using function and nested if-else
+#include<stdio.h>
+void main()
+{
+	float cost,discount,selling;
+	printf("enter the cost price of the book:");
+	scanf("%f",&cost);
+	printf("enter the discount percentage:");
+	scanf("%f",&discount);
+	printf("the selling price of the book is:%.2f\n",selling);
+}
