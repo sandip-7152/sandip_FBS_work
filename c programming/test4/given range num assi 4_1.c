492c8d9c9966ba034dbbded97a7c34c03f9282b7
sandip rathod <sandiprathod41714@gmail.com>
2025-08-06 15:21:00 +0530

    Add files via upload

---
+++ c programming/test4/given range num assi 4_1.c
@@ -0,0 +1,24 @@
+//print prime numbers in the given range 1 to n
+#include<stdio.h>
+void mani()
+
+{
+		int n=100;
+	int sum = 1 ;
+	printf("enter the value of n :");
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++)
+	{
+		sum += i;
+	}
+	printf("prime numbers from 1 to % are:\n",n);
+	{
+	
+		if("is prime num 1 to n ")
+		{
+			printf("%d",n);
+		}
+		scanf("%d",&n);
+	}
+	printf("%d \n 1 to n");
+}
