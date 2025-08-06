addbc5a6e08c8b935b768bd1d5997f7080f12e30
sandip rathod <sandiprathod41714@gmail.com>
2025-08-06 15:25:52 +0530

    Add files via upload

---
+++ c programming/test6/find common element.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n1,n2;
+	printf("enter the of first array");
+	scanf("%d",n1);
+for(int i=1;i<=n1;i++)
+{
+for(int j=1;j<=n2;j++)
+	printf("enter the of second array");
+	scanf("%d",n2);
+printf("\n");
+}
+}
