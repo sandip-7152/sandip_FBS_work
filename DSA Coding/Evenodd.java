96bc7a1437b97a956e247027cd40034e6d9a6df6
sandip rathod <sandiprathod41714@gmail.com>
2026-05-18 14:16:10 +0530

    Add files via upload

---
+++ DSA Coding/Evenodd.java
@@ -0,0 +1,13 @@
+class Evenodd{
+	public static void main (String[]args){
+	int arr[] = {12,21,34,43,45,54};
+	for(int i=0;i<arr.length;i++){
+		if(arr[i] % 2 == 0){
+	System.out.println("is a Even number" + arr[i]);
+	}
+	else{
+	System.out.println("is a Odd number " + arr[i]);
+	}
+    }
+  }
+}
