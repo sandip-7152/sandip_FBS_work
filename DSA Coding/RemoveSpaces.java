097e5896aa2beb929bb57d905b71cf654aa898d4
Sandeep Rathod <sandiprathod41714@gmail.com>
2026-05-26 17:23:15 +0530

    Add files via upload

---
+++ DSA Coding/RemoveSpaces.java
@@ -0,0 +1,7 @@
+class RemoveSpaces {
+ 	public static void main (String [] args){
+	String name  = " s a n d i p     R a t h o d ";
+		name = name.replace(" ","");
+	System.out.println(name);
+	}
+}
