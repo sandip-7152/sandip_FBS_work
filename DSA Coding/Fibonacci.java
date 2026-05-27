8235d231b7f3ba15aad0465d503270ab8a07d1ca
Sandeep Rathod <sandiprathod41714@gmail.com>
2026-05-27 18:09:16 +0530

    Add files via upload

---
+++ DSA Coding/Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Fibonacci {
+    public static void main(String[] args) {
+        int term, a = 0, b = 1, c;
+
+        System.out.print("Enter the term: ");
+        Scanner f = new Scanner(System.in);
+        term = f.nextInt();
+
+        for (int i = 1; i <= term; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
