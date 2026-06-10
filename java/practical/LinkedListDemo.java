d9e3f992d492c0dbd9f615725110f2d221f8de8e
Sandeep Rathod <sandiprathod41714@gmail.com>
2026-06-10 17:12:11 +0530

    Add files via upload

---
+++ java/practical/LinkedListDemo.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Animal");
+        list.add("hours");
+        list.add("Tiger");
+
+        System.out.println(list);
+    }
+}
